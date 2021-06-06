fe0d926bb0a3556fcc3e37f80aae8ecd84733e8a
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2021-06-06 18:34:32 -0300

    atualização da nota

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,22 +1,34 @@
 class NotesController < ApplicationController
-
+    #before_action :authenticate_user!
+    #protect_from_forgery prepend: true
     skip_before_action :verify_authenticity_token
 
     def index 
         @notes = Note.all
     end
 
-    def new
-        
-    end
-
     def create
         @note = Note.new(title: notes_params[:title],
                          note: notes_params[:note],
                          priority: notes_params[:priority],
-                         user_id: 5)
+                         user_id: current_user.id)
         @note.save
     end
+
+    def edit
+        @note = Note.find_by(id: params[:id])
+        puts @note[:title]
+    end
+
+    def update
+        @note = Note.find_by(id: params[:id])
+        @note.update(notes_params)
+    end
+
+    def delete
+    end
+    
+    private
 
     def notes_params
         params.require(:notes).permit(:title, :note, :priority)
---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,9 @@
+module SessionHelper
+    def sign_in
+        session[:user_id] = @user.id
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+end
